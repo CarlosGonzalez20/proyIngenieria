965675152d9cb594f72dcdb87fe17a21d3ccc3d0
CarlosGonzalez20 <ll3224645@gmail.com>
2025-05-08 17:03:59 -0600

    cambio de connection query por pool query

--- config-db/gestion-horarios.js
+++ config-db/gestion-horarios.js
@@ -3,20 +3,24 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('❌ Error al conectar con la base de datos:', err.message);
   } else {
     console.log('✅ Conexión a la base de datos MySQL exitosa');
+    connection.release();
   }
 });
 
-export default db;
+export default pool;
